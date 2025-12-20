820fe3e1ce849da5b3a8735c3baf81fb8172796b
Princeme04 <ovankkusumadewa@gmail.com>
2025-12-21 06:16:05 +0700

    update

--- CineRator/src/main/java/cinerator/Main.java
+++ CineRator/src/main/java/cinerator/Main.java
@@ -13,7 +13,7 @@ public class Main {
         // 1. Setup Error Catcher (Safety Net)
         // This forces any hidden UI crashes to print to the console
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            System.err.println("!!! GUI ERROR !!!");
+            System.err.println("!!! GUI ERRO !!!");
             e.printStackTrace();
         });
 
